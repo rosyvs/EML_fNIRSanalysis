4db9415407262d3717fe7bbd3364d5ce6642586e
tjgran01 <tjgran01@syr.edu>
2021-04-27 12:24:36 -0600

    updated readme.md

---
+++ utilities/generic/trigger_decomposer.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+class TriggerDecomposer(object):
+    def __init__(self):
+        pass
+
+
+    def decompose_trigger(self, trigger_to_decomp, trigger_df):
+        pass
+
+
+
+if __name__ == "__main__":
+    print("Under Construction.")
